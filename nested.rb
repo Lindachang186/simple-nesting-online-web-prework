6400aac11dd8301d3689797864047f68e94de865
Linda Chang <lindachang186@gmail.com>
2018-10-16 00:29:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:known_for] = "Assembly"
+		 new_language = "Assembly"
+programmer_hash[:dennis_ritchie][:known_for] = new_language
 return programmer_hash
 end
